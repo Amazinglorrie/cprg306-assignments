0fb5a96c81bc51801a30cd350b9bbdf4639229a4
LORETTA OKOH <amazinglorrie@gmail.com>
2026-01-22 20:12:38 -0700

    cprg306-assignments

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Loretta Okoh</p>
+      <Link href="https://github.com/Amazinglorrie/your-repo-name">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from './StudentInfo';
+
+export default function Week2() {
+  return (
+    <main>
+      <h1>Week 2</h1>
+      <StudentInfo />
+    </main>
+  );
+}
