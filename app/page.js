d942367609ec96f8da3ca391c70ee64f6f0f3aba
LORETTA OKOH <amazinglorrie@gmail.com>
2026-01-23 09:13:22 -0700

    updated the code in apppage

--- app/page.js
+++ app/page.js
@@ -4,7 +4,13 @@ export default function Page() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Go to Week 2</Link>
+      <Link href="/week-2">Go to Week 2 → </Link>
+      <Link href="/week-3">Go to Week 3 → </Link>
+      <Link href="/week-4">Go to Week 4 → </Link>
+      <Link href="/week-5">Go to Week 5 → </Link>
+      <Link href="/week-6">Go to Week 6 → </Link>
+      <Link href="/week-7">Go to Week 7 → </Link>
+      <Link href="/week-8">Go to Week 8 → </Link>
     </main>
   );
 }
