a35c91515c0561e05452edd4978e6050f0e5163f
LORETTA OKOH <amazinglorrie@gmail.com>
2026-03-20 10:47:54 -0600

    Use environment variables for Firebase config

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbYhmjtzvjM4Bq2Vz2s0_legcPoSksouw",
-  authDomain: "cprg306-assignments-552ba.firebaseapp.com",
-  projectId: "cprg306-assignments-552ba",
-  storageBucket: "cprg306-assignments-552ba.firebasestorage.app",
-  messagingSenderId: "204500759633",
-  appId: "1:204500759633:web:57983cc15d47bbd164f470"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
